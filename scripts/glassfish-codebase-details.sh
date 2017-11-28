c624bf4874aab03035428446bc2450fa319115bb
Vinay Vishal <vinay.vishal@oracle.com>
2017-11-28 19:55:49 +0530

    Enhance script with file breakup for each module

--- scripts/glassfish-codebase-details.sh
+++ scripts/glassfish-codebase-details.sh
@@ -30,12 +30,56 @@ echo "</html>"
 }
 
 module_details(){
+EXTENSIONS=()
+FILE_TYPES=""
+FILE_EXTENSION=""
+
 
 echo "<table border=\"1\">"
 echo "<tr>"
 echo "<th>Type</th>"
 echo "<th>Count</th>"
 echo "</tr>"
+INDEX=0
+COUNTER=0
+for file in `find $1`
+do
+ if [ -f $file ];then 
+#  echo "$file"
+  FILE_EXTENSION=`echo ${file##*\.}`
+  if [ ! -z ${FILE_EXTENSION} ];then
+#    echo $FILE_EXTENSION
+    EXTENSIONS[$INDEX]=$FILE_EXTENSION    
+    INDEX=`expr $INDEX + 1`
+  fi
+ fi
+done
+
+#for i in "${EXTENSIONS[@]}"
+#do
+#  echo "<tr>"
+#  echo "<td>$i</td>"
+#  echo "<td>-</td>"
+#  echo "</tr>"
+#done 
+
+IFS=$'\n' EXTENSION_SET=($(sort -u <<<"${EXTENSIONS[*]}"))
+unset IFS
+for ext in “${EXTENSION_SET[@]}”
+#COUNTER=`expr 0`
+do
+  for exts in "${EXTENSIONS[@]}"
+  do
+    if [ $exts = $ext ];then
+    COUNTER=`expr $COUNTER + 1`
+    fi
+  done 
+  echo "<tr>"
+  echo "<td>$ext</td>"
+  echo "<td>$COUNTER</td>"
+  echo "</tr>"
+COUNTER=0
+done 
 echo "</table>"
 
 }
@@ -46,7 +90,7 @@ cat > ./glassfish-stats.html <<EOF
 
 $(html_header)
 $(html_body $1)
-$(html_footer)
+i$(html_footer)
 
 EOF
 }
@@ -60,7 +104,6 @@ TOTAL_COUNT=0
 for pom in `find $1 -name "pom.xml"`
 do
   COUNT=0
-  FILE_TYPES=""
   #echo $pom
   PARENT_DIR="${pom%/*}"
   echo "<tr>"
@@ -71,14 +114,6 @@ do
   echo "<td>$COUNT</td>" 
   echo "<td>$(module_details $PARENT_DIR)</td>"
   echo "</tr>"
-#  echo Total number of files in directory `find "$PARENT_DIR" -type f | wc -l`
-  for file in `ls $PARENT_DIR`
-  do
-   if [ -f $file ];then 
-   echo "File extension is: ${file##*\.}"
-   fi
-  done 
-  #echo $PARENT_DIR
 done
 echo "<tr><td>"-"</td><td>"-"</td><td>$TOTAL_COUNT</td><td>"-"</td></tr>"
 }
